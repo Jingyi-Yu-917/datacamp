14d6aeae52bf55221467ad0e170f694f6eb638f8
Jingyi Yu <y_j_y7139@163.com>
2022-02-22 20:25:27 +0000

    group

--- datacamp analysis.R
+++ datacamp analysis.R
@@ -2,3 +2,7 @@ library(dplyr)
 library(readr)
 read_csv('~/Desktop/Rstudio/life-under-lockdown-wave2-extract.csv')
 lul2_d <- read_csv('~/Desktop/Rstudio/life-under-lockdown-wave2-extract.csv')
+
+lul2_d %>%
+  group_by(know_facebook) %>%
+  summarise(n=n(), beliefs=mean(beliefs))
